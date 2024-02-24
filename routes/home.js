2ba8eb26194f97ffc2d557d00028ba5e3f0ccf9c
Sidd@2124 <siddhumsd@gmail.com>
2024-02-25 02:17:39 +0530

    Updates

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,65 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
+let products = [
+  {
+    id: "1",
+    name: "Naresh",
+    number: "9666841615",
+    imageURL: "https://i.ibb.co/4dcxwgY/Screenshot-2024-02-23-174428.png",
+    insuranceDocument: "",
+    insuranceDate: "07/07/2023",
+    adharDocumentFront: "",
+    adharDocumentBack: "",
+    insuranceNo: "001",
+    aavuFront: "",
+    aavuBack: "",
+    aavuRight: "",
+    aavuLeft: ""
+  },
+];
+
+// GET all products
+router.get('/products', (req, res) => {
+  res.json(products);
+});
+
+// POST a new product
+router.post('/products', (req, res) => {
+  const {
+    id,
+    name,
+    number,
+    imageURL,
+    insuranceDocument,
+    insuranceDate,
+    adharDocumentFront,
+    adharDocumentBack,
+    insuranceNo,
+    aavuFront,
+    aavuBack,
+    aavuRight,
+    aavuLeft
+  } = req.body;
+
+  const newProduct = {
+    id,
+    name,
+    number,
+    imageURL,
+    insuranceDocument,
+    insuranceDate,
+    adharDocumentFront,
+    adharDocumentBack,
+    insuranceNo,
+    aavuFront,
+    aavuBack,
+    aavuRight,
+    aavuLeft
+  };
+  products.push(newProduct);
+
+  res.status(201).json({ message: 'Product added successfully', product: newProduct });
 });
 
 module.exports = router;
