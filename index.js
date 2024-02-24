0320a58068c94b69ab338b103914712d8fd81d4c
Sidd@2124 <siddhumsd@gmail.com>
2024-02-25 02:55:41 +0530

    Updates

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser'); // Import bodyParser
+const app = express();
 const homeRouter = require("./routes/home");
 
 // Middlewares
-const app = express();
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' })); // Use bodyParser with JSON payload limit
 app.use(express.json());
 
 // Routes
 app.use("/", homeRouter);
 
 // connection
-const port = process.env.PORT || 4001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+const port = process.env.PORT || 3004;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
