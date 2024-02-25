50cb81ab6ca2788a667613e9e98de750fa2e65ba
Sidd@2124 <siddhumsd@gmail.com>
2024-02-25 16:39:50 +0530

    Updates

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
 const app = express();
@@ -9,17 +8,14 @@ const app = express();
 app.use(cors());
 
 // Parse JSON bodies with increased payload limit (e.g., 50MB)
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 
 // Create a MySQL connection
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'Siddu',
   password: 'Sidd@2124',
-  database: 'products.products',
-  authPlugins: {
-    mysql_clear_password: () => () => Buffer.from('Sidd@2124')
-  }
+  database: 'products'
 });
 
 // Connect to MySQL
@@ -32,18 +28,17 @@ connection.connect((err) => {
 });
 
 // Define routes
-app.get('/api/products', (req, res) => { // Updated route to /api/products
-  connection.query('SELECT * FROM products', (error, results, fields) => {
+app.get('/api/products', (req, res) => {
+  connection.query('SELECT * FROM data', (error, results, fields) => {
     if (error) {
       console.error('Error retrieving products: ' + error.message);
-      res.status(500).json({ error: 'Error retrieving products' });
-      return;
+      return res.status(500).json({ error: 'Error retrieving products' });
     }
     res.json(results);
   });
 });
 
-app.post('/api/products', (req, res) => { // Updated route to /api/products
+app.post('/api/products', (req, res) => {
   const {
     id,
     name,
@@ -60,14 +55,13 @@ app.post('/api/products', (req, res) => { // Updated route to /api/products
     aavuLeft
   } = req.body;
 
-  const sql = 'INSERT INTO products (id, name, number, imageURL, insuranceDocument, insuranceDate, adharDocumentFront, adharDocumentBack, insuranceNo, aavuFront, aavuBack, aavuRight, aavuLeft) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  const sql = 'INSERT INTO data (id, name, number, imageURL, insuranceDocument, insuranceDate, adharDocumentFront, adharDocumentBack, insuranceNo, aavuFront, aavuBack, aavuRight, aavuLeft) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
   const values = [id, name, number, imageURL, insuranceDocument, insuranceDate, adharDocumentFront, adharDocumentBack, insuranceNo, aavuFront, aavuBack, aavuRight, aavuLeft];
 
   connection.query(sql, values, (error, results, fields) => {
     if (error) {
       console.error('Error inserting product: ' + error.message);
-      res.status(500).json({ error: 'Error inserting product' });
-      return;
+      return res.status(500).json({ error: 'Error inserting product' });
     }
     console.log('Product added successfully');
     res.status(201).json({ message: 'Product added successfully', product: req.body });
